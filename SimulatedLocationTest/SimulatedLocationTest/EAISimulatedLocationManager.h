ccd0f92c18f09cc2db26e77087706e98e7522874
Eric Ito <eric.ito@gmail.com>
2013-10-29 11:06:25 -0700

    add link to original file

--- SimulatedLocationTest/SimulatedLocationTest/EAISimulatedLocationManager.h
+++ SimulatedLocationTest/SimulatedLocationTest/EAISimulatedLocationManager.h
@@ -1,9 +1,6 @@
 //
 //  EAISimulatedLocationManager.h
-//  SimulatedLocationTest
-//
-//  Created by Eric Ito on 10/27/13.
-//  Copyright (c) 2013 Eric Ito. All rights reserved.
+//  Pulled from here: https://github.com/eito/ios-sdk-tests
 //
 
 #import <CoreLocation/CoreLocation.h>
